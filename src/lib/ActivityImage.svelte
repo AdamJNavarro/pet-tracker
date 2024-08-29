9e98b2f03f36a0f334ea6e207ba05ca138ad15d7
Adam Navarro <adamjnav@gmail.com>
2024-08-29 12:47:00 -0500

    add meds image and update activity type

--- src/lib/ActivityImage.svelte
+++ src/lib/ActivityImage.svelte
@@ -3,11 +3,15 @@
 	import BathedImage from '$assets/bathed.png';
 	import GroomedImage from '$assets/groomed.png';
 	import WalkedImage from '$assets/walked.png';
+	import FedImage from '$assets/fed.png';
+	import MedsImage from '$assets/meds.png';
 	import type { Activity } from '$lib';
 
 	export let name: Activity;
 
 	function get_img_src(name: Activity): string {
+		if (name === 'fed') return FedImage;
+		if (name === 'meds') return MedsImage;
 		if (name === 'bathed') return BathedImage;
 		if (name === 'groomed') return GroomedImage;
 		if (name === 'outside') return OutsideImage;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,2 @@
 // place files you want to import through the `$lib` alias in this folder.
-export type Activity = 'bathed' | 'fed' | 'groomed' | 'outside' | 'walked';
+export type Activity = 'bathed' | 'fed' | 'groomed' | 'meds' | 'outside' | 'walked';
