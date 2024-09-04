b6c490fdd1ddd8336bc9eeb35619fba4475f5ed9
Adam Navarro <adamjnav@gmail.com>
2024-09-04 15:06:55 -0500

    start diverging drizzle config for dev and prod

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const is_dev = process.env.DB_ENV === 'dev';
 
 export default defineConfig({
 	schema: './src/server/db/schema.ts',
 	dbCredentials: {
 		url: process.env.DATABASE_URL
 	},
-	out: './src/server/db/migrations',
+	out: is_dev ? './src/server/db/migrations/dev' : './src/server/db/migrations/prod',
 	verbose: true,
 	strict: true,
 	dialect: 'postgresql'
