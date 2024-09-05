453a5075676ad1cf4643892b8da708b0704b6c0e
Adam Navarro <adamjnav@gmail.com>
2024-09-05 12:16:53 -0500

    set db url connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.PROD_DB_URL) throw new Error('PROD DB URL is not set');
+if (!process.env.DEV_DB_URL) throw new Error('DEV DB URL is not set');
 const is_dev = process.env.DB_ENV === 'dev';
+console.log('is_dev', is_dev);
+const db_url = is_dev ? process.env.DEV_DB_URL : process.env.PROD_DB_URL;
+console.log('db_url', db_url);
 
 export default defineConfig({
 	schema: './src/server/db/schema.ts',
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: db_url
 	},
 	out: is_dev ? './src/server/db/migrations/dev' : './src/server/db/migrations/prod',
 	verbose: true,
