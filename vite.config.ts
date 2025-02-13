e165c5967cf4cbf6edccef638c7225c1be96e3ba
Adam Navarro <adamjnav@gmail.com>
2025-02-13 13:34:14 -0600

    use env var in vite server open config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
-export default defineConfig({
-	server: {
-		port: 5175,
-		strictPort: true,
-		open: '/?code=124'
-	},
-	plugins: [
-		enhancedImages(),
-		sveltekit(),
-		Icons({
-			compiler: 'svelte'
-		})
-	]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		server: {
+			port: 5175,
+			strictPort: true,
+			open: `/?code=${env.PUBLIC_AUTH_CODE}`
+		},
+		plugins: [
+			enhancedImages(),
+			sveltekit(),
+			Icons({
+				compiler: 'svelte'
+			})
+		]
+	};
 });
